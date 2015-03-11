88d8037f4a24b31626b6c741b38b5f0747d90c02
Dave Young <dave@blueshellgames.com>
2015-03-11 20:39:36 +0000

    spaces/tabs

--- git_fat/git_fat.py
+++ git_fat/git_fat.py
@@ -433,11 +433,11 @@ try:
                             # download is removed.
                             os.remove(localfn)
                             raise
-						except Exception, e:
-							# re-download this file on any error
-							logging.warning('S3 pull of %(fn)s failed: %(e)s; re-queueing download in 5 seconds' % locals())
-							gevent.sleep(5)
-							pool.spawn(_pull_parallel_one, stats(fb)
+                        except Exception, e:
+                            # re-download this file on any error
+                            logging.warning('S3 pull of %(fn)s failed: %(e)s; re-queueing download in 5 seconds' % locals())
+                            gevent.sleep(5)
+                            pool.spawn(_pull_parallel_one, stats(fb)
             for f in files:
                 pool.spawn(_pull_parallel_one, stats, f)
             pool.join()
